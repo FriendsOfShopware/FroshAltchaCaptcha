1e6dd00b1bf0d1e256f1431c10f002e0cd3f5d2a
tinect <s.koenig@tinect.de>
2025-11-20 21:41:45 +0100

    fix: missing init method produces error

--- src/Resources/app/storefront/src/plugin/altcha.plugin.js
+++ src/Resources/app/storefront/src/plugin/altcha.plugin.js
@@ -3,4 +3,6 @@ import 'altcha/i18n/de';
 import 'altcha/i18n/nl';
 import 'altcha/i18n/fr-fr';
 
-export default class AltchaPlugin extends window.PluginBaseClass {}
+export default class AltchaPlugin extends window.PluginBaseClass {
+    init() {}
+}
