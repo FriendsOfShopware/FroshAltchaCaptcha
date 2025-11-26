55af50cde6f7c892a869fc864fdecc3cfaed3852
tinect <s.koenig@tinect.de>
2025-11-26 16:02:37 +0100

    feat: add option to specify the auto mode

---
+++ src/Migration/Migration1764168394AddSubmitOption.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frosh\AltchaCaptcha\Migration;
+
+use Doctrine\DBAL\Connection;
+use Frosh\AltchaCaptcha\Storefront\Framework\AltchaCaptcha;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1764168394AddSubmitOption extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1764168394;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $captchaItems = [
+            AltchaCaptcha::CAPTCHA_NAME => [
+                'config' => [
+                    'autoMode' => 'onsubmit',
+                ],
+            ],
+        ];
+
+        $connection->executeStatement(
+            'UPDATE system_config SET configuration_value = JSON_MERGE_PATCH(
+                configuration_value,
+                :captchaItems
+            ) WHERE configuration_key = :key',
+            [
+                'captchaItems' => \json_encode(['_value' => $captchaItems], \JSON_THROW_ON_ERROR),
+                'key' => 'core.basicInformation.activeCaptchasV2',
+            ]
+        );
+    }
+}
