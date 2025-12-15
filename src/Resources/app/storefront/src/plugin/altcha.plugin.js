16626768a32d375c8674094fbc551dd2c185e60c
tinect <s.koenig@tinect.de>
2025-12-16 00:31:13 +0100

    feat: add config for invisible mode
    Closes #1

--- src/Resources/app/storefront/src/plugin/altcha.plugin.js
+++ src/Resources/app/storefront/src/plugin/altcha.plugin.js
@@ -4,5 +4,17 @@ import 'altcha/i18n/nl';
 import 'altcha/i18n/fr-fr';
 
 export default class AltchaPlugin extends window.PluginBaseClass {
-    init() {}
+    init() {
+        this.el.addEventListener('statechange', this.onStateChange.bind(this));
+    }
+
+    onStateChange(event) {
+        if (!('detail' in event)) {
+            return;
+        }
+
+        if (['verifying', 'verified'].includes(event.detail.state) === false) {
+            this.el.classList.remove('d-none');
+        }
+    }
 }
