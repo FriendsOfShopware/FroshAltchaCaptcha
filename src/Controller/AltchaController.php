736dedf93f390e3cae486c74b81c138ac3322f54
tinect <s.koenig@tinect.de>
2026-06-17 22:26:12 +0200

    feat: change behaviour of whitelisted customer to a simpler captcha

--- src/Controller/AltchaController.php
+++ src/Controller/AltchaController.php
@@ -6,6 +6,8 @@ use AltchaOrg\Altcha\Algorithm\Pbkdf2;
 use AltchaOrg\Altcha\Altcha;
 use AltchaOrg\Altcha\CreateChallengeOptions;
 use Frosh\AltchaCaptcha\Storefront\Framework\AltchaCaptcha;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,17 +23,22 @@ class AltchaController extends AbstractController
     }
 
     #[Route('/altcha/challenge', name: 'widgets.altcha.challenge')]
-    public function getCaptcha(): JsonResponse
+    public function getCaptcha(SalesChannelContext $context): JsonResponse
     {
         try {
             $secretKey = $this->systemConfigService->getString(
                 AltchaCaptcha::CONFIG_PATH . '.' . AltchaCaptcha::CONFIG_FIELD_SECRET
             );
 
+            $isWhitelisted = $this->systemConfigService->getBool(
+                AltchaCaptcha::CONFIG_PATH . '.whitelistCustomers',
+                $context->getSalesChannelId()
+            ) && $this->isNonGuestCustomer($context);
+
             $challengeOptions = new CreateChallengeOptions(
                 algorithm: new Pbkdf2(),
-                cost: 5000,
-                counter: \random_int(1000, 1200),
+                cost: $isWhitelisted ? 1 : 5000,
+                counter: $isWhitelisted ? 1 : \random_int(1000, 1200),
                 expiresAt: new \DateTimeImmutable('+5 minutes')
             );
 
@@ -41,5 +48,12 @@ class AltchaController extends AbstractController
         } catch (\Throwable $e) {
             return new JsonResponse($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private function isNonGuestCustomer(SalesChannelContext $context): bool
+    {
+        $customer = $context->getCustomer();
+
+        return $customer instanceof CustomerEntity && $customer->getGuest() === false;
     }
 }
